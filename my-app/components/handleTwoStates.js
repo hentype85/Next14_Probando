adfa00f1d3b63cf0c2c267cacde82ae8844739b8
martin <martinleiro9@gmail.com>
2024-05-05 10:56:24 -0300

    paginacion useEffect

--- my-app/components/handleTwoStates.js
+++ my-app/components/handleTwoStates.js
@@ -4,46 +4,68 @@ import React, { useState, useEffect } from 'react';
 import './component.css';
 
 export default function HandleTwoStates() {
-  const [datos, setData] = useState(null); // datos de la API
+  // estados
+  const [datos, setData] = useState([]); // datos de la API
   const [error, setError] = useState(null); // error al obtener los datos
+  // estados para la paginacion
+  const [currentPage, setCurrentPage] = useState(1); // pagina actual
+  const itemsPerPage = 10; // items por pagina
 
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos?_page=${currentPage}&_limit=${itemsPerPage}`) //  obtengo datos de la API por pagina
+      .then(response => response.json()) // convierto respuesta en JSON
+      .then(jsonData => { // datos obtenidos de respuesta
+        setData(jsonData); // cargo los datos
+        setError(null); // limpio el error
+      })
+      .catch(error => {
+        setError(error.message); // si hay error, lanzo excepcion
+      });
+  }, [currentPage]); // ejecuto cuando cambia la pagina
+
+/*
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos'); // obtengo datos de la API
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=${currentPage}&_limit=${itemsPerPage}`);
         if (!response.ok) {
-          throw new Error('Error al obtener los datos'); // si hay error, lanzo excepcion
+          throw new Error('Error al obtener los datos');
         }
 
-        const jsonData = await response.json(); // convierto respuesta en JSON
-
-        setData(jsonData); // cargo los datos
-        setError(null); // limpio el error
-
+        const jsonData = await response.json();
+        setData(jsonData);
+        setError(null);
       } catch (error) {
         setError(error.message);
       }
     };
 
-    fetchData(); // ejecuto funcion
-  }, []);
+    fetchData();
+  }, [currentPage]);
+*/
+  const nextPage = () => {
+    setCurrentPage(prevPage => prevPage + 1);
+  };
+  const prevPage = () => {
+    setCurrentPage(prevPage => prevPage - 1);
+  };
 
-  if (!datos && !error) return <p>Cargando datos...</p>;
-  else if (!datos) return <p>{error}</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <div className="container">
         <ul>
           <h1>useEffect datos API</h1>
-          {error && <li>{error}</li>}
-
-          {datos && datos.map((dato) => (
+          {datos.map((dato) => (
             <li key={dato.id}>
               {dato.title}
             </li>
           ))}
-
         </ul>
+        <div className="pagination">
+          <button onClick={prevPage} disabled={currentPage === 1}>Anterior</button>
+          <button onClick={nextPage} disabled={datos.length < itemsPerPage}>Siguiente</button>
+        </div>
     </div>
   );
 }
