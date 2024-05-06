4ff3d59fe8b873949d6d9f57b5853cc20a84a148
martin <martinleiro9@gmail.com>
2024-05-06 18:17:41 -0300

    fix

--- my-app/components/handleInfiniteScroll.js
+++ my-app/components/handleInfiniteScroll.js
@@ -7,20 +7,16 @@ export default function HandleInfiniteScroll() {
     const [datos, setData] = useState([]); // datos de la API
     const [error, setError] = useState(null); // error al obtener los datos
     const [currentPage, setCurrentPage] = useState(1); // pagina actual
-    const [loading, setLoading] = useState(false); // estado de carga
 
     useEffect(() => {
-        setLoading(true);
         fetch(`https://jsonplaceholder.typicode.com/todos?_page=${currentPage}`)
           .then(response => response.json())
           .then(jsonData => {
             setData(prevData => [...prevData, ...jsonData]); // combino los datos previos y nuevos en un solo array
             setError(null);
-            setLoading(false);
           })
           .catch(error => {
             setError(error.message);
-            setLoading(false);
           });
     }, [currentPage]);
 
@@ -48,7 +44,6 @@ export default function HandleInfiniteScroll() {
                         <li key={dato.id}>{dato.title}</li>
                     ))}
                 </ul>
-                {loading && <div>Loading...</div>}
                 {error && <div>Error: {error}</div>}
             </div>
         </>
