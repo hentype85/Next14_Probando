a1e16f980df4d3e38e13fe6af858ca6f9d54c2fc
martin <martinleiro9@gmail.com>
2024-05-08 12:46:33 -0300

    pruebas manipulacion menus mobil desktop

--- my-app/components/navbar.js
+++ my-app/components/navbar.js
@@ -9,52 +9,87 @@ export default function Navbar() {
 
     useEffect(() => {
         const handleResize = () => {
-            setShowMenu(window.innerWidth > 600);
+            if (window.innerWidth > 600) {
+                setShowMenu(true);
+            } else {
+                setShowMenu(false);
+            }
         };
+
         window.addEventListener('resize', handleResize);
+        handleResize(); // ejecutar al cargar la pagina
+
+        return () => { // limpiar el evento
+            window.removeEventListener('resize', handleResize);
+        };
 
     }, []);
 
+    // manipular DOM
+    // abrir menu burguer en dispositivos moviles
     const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    };
+        let menuBtn = document.querySelector('.menu-btn');
+        let menu = document.querySelector('.nav');
+    
+        menuBtn.addEventListener('click', function() {
+            menuBtn.classList.toggle('active');
+            menu.classList.toggle('active');
+        });
+    }
+
+    // cerrar automaticamente menu burguer en dispositivos moviles
+    const closeToggleMenu = () => {
+        let navLink = document.querySelectorAll('.nav__list_item');
+        let menuBtn = document.querySelector('.menu-btn');
+        let menu = document.querySelector('.nav');
+    
+        navLink.forEach( (n) => {
+            n.addEventListener('click', () => {
+                menuBtn.classList.remove('active');
+                menu.classList.remove('active');
+            });
+        });
+    }
+    // fin manipular DOM
 
     return (
         <>
-        <div className={showMenu ? "menu desktop-menu" : "menu-btn"}>
+        <div className={showMenu ? "" : "menu-btn"} onClick={ () => {toggleMenu();  closeToggleMenu();} }>
             <span className={showMenu ? "" : "bar"}></span>
             <span className={showMenu ? "" : "bar"}></span>
             <span className={showMenu ? "" : "bar"}></span>
         </div>
-        <nav>
-            <ul className={showMenu ? "menu desktop-menu" : ""}>
-                <li className={showMenu ? "menu-item" : ""}>
-                    <Link href="/">
-                        Home
-                    </Link>
-                </li>
-                <li className={showMenu ? "menu-item" : ""}>
-                    <Link href="/login">
-                        Login
-                    </Link>
-                </li>
-                <li className={showMenu ? "menu-item" : ""}>
-                    <Link href="/tienda">
-                        Tienda
-                    </Link>
-                </li>
-                <li className={showMenu ? "menu-item" : ""}>
-                    <Link href="/about">
-                        About
-                    </Link>
-                </li>
-                <li className="menu-item">
-                    <Link href="/ejemplosHooks">
-                        Ejemplos Hooks
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <div class={showMenu ? "" : "header__nav"}>
+            <nav class={showMenu ? "" : "nav"}>
+                <ul className={showMenu ? "menu desktop-menu" : "nav__list"}>
+                    <li className={showMenu ? "menu-item" : "nav__list_item"}>
+                        <Link href="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className={showMenu ? "menu-item" : "nav__list_item"}>
+                        <Link href="/login">
+                            Login
+                        </Link>
+                    </li>
+                    <li className={showMenu ? "menu-item" : "nav__list_item"}>
+                        <Link href="/tienda">
+                            Tienda
+                        </Link>
+                    </li>
+                    <li className={showMenu ? "menu-item" : "nav__list_item"}>
+                        <Link href="/about">
+                            About
+                        </Link>
+                    </li>
+                    <li className={showMenu ? "menu-item" : "nav__list_item"}>
+                        <Link href="/ejemplosHooks">
+                            Ejemplos Hooks
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
         </>
     );
 }
